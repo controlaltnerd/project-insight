b77e7ff882c29d1ff3c8e85f67222e066dfdfe2d
Vu Dao <Vu@Vus-Mac.local>
2025-04-05 08:20:45 -0400

    Fix bug

--- InSight/src/main/java/com/assemblers/app/DatabaseAccess/UpdateRangeSalaryDAO.java
+++ InSight/src/main/java/com/assemblers/app/DatabaseAccess/UpdateRangeSalaryDAO.java
@@ -5,20 +5,24 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class UpdateRangeSalaryDAO {
-    public static void updateRangeSalary(int min, int max, int increment){
+    public static int updateRangeSalary(int min, int max, int increment){
         String query = "UPDATE employees " +
-                   "SET Salary = Salary * ? / 100" +
+                   "SET Salary = Salary + Salary * ? / 100 " +
                    "WHERE Salary BETWEEN ? AND ?";
     
     try (Connection conn = DatabaseAccessHelper.getConnection();
          PreparedStatement stmt = conn.prepareStatement(query)) {
         
-        stmt.setInt(1, min);
-        stmt.setInt(2, max);
-        stmt.setInt(3, increment);
+        stmt.setInt(1, increment);
+        stmt.setInt(2, min);
+        stmt.setInt(3, max);
+
+        int rowsUpdated = stmt.executeUpdate();
+        return rowsUpdated;
         
     } catch (SQLException e) {
         e.printStackTrace();
     }
+    return 0;
     }
 }
