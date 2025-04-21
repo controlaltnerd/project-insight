534e37077d9a4832caf696d679d3204316c82f8a
controlaltnerd <112291773+controlaltnerd@users.noreply.github.com>
2025-04-21 09:37:09 -0400

    Removing non-existent import and class

--- InSight/src/main/java/com/assemblers/app/App.java
+++ InSight/src/main/java/com/assemblers/app/App.java
@@ -2,13 +2,10 @@ package com.assemblers.app;
 
 import javax.swing.SwingUtilities;
 
-// import com.assemblers.app.UI.LoginForTestingOnly;
-
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                // new LoginForTestingOnly().setVisible(true);
                 System.out.println("Hello, world!");
             }
         });
