229ec449c015f8ef7d8c65bf352a7f71ab797a46
controlaltnerd <112291773+controlaltnerd@users.noreply.github.com>
2025-04-21 08:54:25 -0400

    Commenting out failing code in App.java

--- InSight/src/main/java/com/assemblers/app/App.java
+++ InSight/src/main/java/com/assemblers/app/App.java
@@ -2,16 +2,14 @@ package com.assemblers.app;
 
 import javax.swing.SwingUtilities;
 
-import com.assemblers.app.UI.LoginForTestingOnly;
+// import com.assemblers.app.UI.LoginForTestingOnly;
 
-/**
- * Hello world!
- */
 public class App {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new LoginForTestingOnly().setVisible(true);
+                // new LoginForTestingOnly().setVisible(true);
+                System.out.println("Hello, world!");
             }
         });
     }
